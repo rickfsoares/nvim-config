359b335b9df7fbe503b1db22ed1461c05efa0539
ricardo <ricardofs547@gmail.com>
2024-01-18 18:54:17 -0300

    first commit

---
+++ lua/core/scripts.lua
@@ -0,0 +1,13 @@
+-- SCRIPTS
+
+-- Colorscheme
+cmd("colorscheme everforest")
+
+-- Format on save (Commented out because Conform.nvim takes care of this)
+--cmd("autocmd BufWritePre * lua vim.lsp.buf.format()")
+
+-- Run ":so" after writing .zshrc
+cmd("autocmd BufWritePost ~/.zshrc so %")
+
+-- Run "xrdb" after writing .Xresources
+cmd("autocmd BufWritePost ~/.Xresources !xrdb %")
---
+++ lua/core/setups.lua
@@ -0,0 +1,34 @@
+-- SETUPS
+
+-- DAP UI
+require("dapui").setup()
+
+-- Alpha start screen / dashboard
+require("alpha").setup(require("alpha.themes.dashboard").config)
+require("alpha.themes.dashboard").section.footer.val = require("alpha.fortune")() -- Quotes
+
+-- Everforest theme
+require("everforest").setup({
+    italics = true,
+})
+
+-- Lualine status bar
+require("lualine").setup({
+    options = {
+        theme = "everforest",
+        component_separators = " ",
+        section_separators = { left = "", right = "" },
+    },
+})
+
+-- Nvim Autopairs
+require("nvim-autopairs").setup()
+
+-- Line highlighting
+require("ibl").setup({
+    indent = { char = "┊" },
+    scope = { enabled = false },
+})
+
+-- Hex color highlighting
+require("colorizer").setup()
