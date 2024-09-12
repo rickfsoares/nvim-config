756334ddf06cfb64883cdf5096c9867688d3069b
ricardo frança soares <rickfsoaresdev@gmail.com>
2024-09-12 07:06:48 -0300

    err map added

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -24,3 +24,6 @@ vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+
+-- Show errors
+vim.keymap.set('n', '<leader>err',function() vim.diagnostic.open_float(0, {scope="line"}) end)
