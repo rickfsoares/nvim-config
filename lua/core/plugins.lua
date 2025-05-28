a9ef4b588380e2453a10e85780fcecebd529fe3e
ricardo frança soares <rickfsoaresdev@gmail.com>
2025-05-28 17:18:15 -0300

    feat: add nvim-java plugin

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -52,6 +52,16 @@ return {
     "williamboman/mason-lspconfig.nvim", -- lspconfig integration
     "neovim/nvim-lspconfig",             -- LSP configuration
 
+    ------- NVIM-JAVA
+    {
+        "nvim-java/nvim-java",
+        config = function ()
+            require('java').setup()
+            require('lspconfig').jdtls.setup({})
+        end
+
+    },
+
     ------- Debugging
     {
         "rcarriga/nvim-dap-ui", -- Debugger
