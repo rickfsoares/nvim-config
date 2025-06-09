ec2643ef69f5e20bf884495872b410d006089cbe
ricardo frança soares <rickfsoaresdev@gmail.com>
2025-06-09 13:42:29 -0300

    feat: add remove logic in harpoon

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -12,8 +12,37 @@ vim.keymap.set("n", "<M-2>", function() harpoon:list():select(2) end)
 vim.keymap.set("n", "<M-3>", function() harpoon:list():select(3) end)
 vim.keymap.set("n", "<M-4>", function() harpoon:list():select(4) end)
 
-vim.keymap.set("n", "<C-d>", function ()
-    harpoon.list():remove(harpoon.list():get_current())
+vim.keymap.set("n", "<M-0>", function ()
+    local index = tonumber(vim.fn.input("Harpoon index o remove: "))
+
+    if not index then
+        print("Invalid index.")
+        return
+    end
+
+    local list = harpoon:list()
+    local items = list.items
+
+    if not items[index] then
+        print("No item at index " .. index)
+        return
+    end
+
+    local new_items = {}
+    for i, item in ipairs(items) do
+        if i ~= index then
+            table.insert(new_items, item)
+        end
+    end
+
+    list.items = {}
+
+    for _, item in ipairs(new_items) do
+        list:add(item)
+    end
+
+    print("Item " .. index .. " removed.")
+
 end)
 
 -- Toggle previous & next buffers stored within Harpoon list
