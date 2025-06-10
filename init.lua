a8f27a6d464e5e09ef11ae46dc7c192ccb49158d
ricardo frança soares <rickfsoaresdev@gmail.com>
2025-06-10 15:52:19 -0300

    fix: sort plugins order

--- init.lua
+++ init.lua
@@ -14,16 +14,14 @@ g.mapleader = " "
 
 -- Imports
 require("core.lazy")
-require("core.plugins")
 require("core.settings")
 require("core.mappings")
 require("core.scripts")
 require("core.themeconfig")
 
-require("plugins.lsp-config-setup") -- LSPconfig setup
 require("plugins.mason")            -- Mason LSP & DAP servers
+require("plugins.lsp-config-setup") -- LSPconfig setup
 require("plugins.nvim-cmp")         -- Autocompletion
 require("plugins.treesitter")       -- Treesitter syntax highlighting
 require("plugins.dap")              -- Debug Adapter Protocol
 require("plugins.harpoon")          -- Harpoon setup
-require("ftplugin.java")            -- JDTLS setup
